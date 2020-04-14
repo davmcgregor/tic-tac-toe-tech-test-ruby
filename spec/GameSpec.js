7ec872695ecfca84df0dfc880470b3e4cda71564
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-04-15 00:23:23 +0100

    Add first test for Game object

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,5 +1,13 @@
 'use strict';
 
 describe('Game', function() {
- 
+  var game
+
+  beforeEach(function() {
+    game = new Game();
+  });
+
+  it('should be a defined object', function() {
+    expect(game instanceof(Game)).toBe(true);
+  });
 });
