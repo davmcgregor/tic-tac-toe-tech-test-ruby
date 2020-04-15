65cf2994957dc052c460ca37a841e4464e89ec89
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-04-15 09:10:27 +0100

    Add properties to Game object

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -4,10 +4,19 @@ describe('Game', function() {
   var game
 
   beforeEach(function() {
-    game = new Game();
+    game = new Game("Jack", "Jill");
   });
 
   it('should be a defined object', function() {
     expect(game instanceof(Game)).toBe(true);
   });
+
+  it('should have a board property', function(){
+    expect(game.board instanceof(Board)).toBe(true);
+  })
+
+  it('should have two player properties', function(){
+    expect(game.player_one).toBeDefined();
+    expect(game.player_two).toBeDefined();
+  })
 });
--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,9 @@
 (function(exports) {
 
-  function Game() {
+  function Game(player_one, player_two, board = new Board()) {
+    this.board = board;
+    this.player_one = player_one;
+    this.player_two = player_two;
 
   }
 
