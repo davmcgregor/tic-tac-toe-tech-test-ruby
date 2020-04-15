6479dcc1f35795fec634662ac1760f6e135515fa
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-04-15 17:33:50 +0100

    Refactor SpecHelper

--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -5,4 +5,12 @@ var playMoves = function(game, one, two, three, four) {
   game.play(four);
 };
 
-var Player1 = "Player 1 wins!"
+var round1 = function(game, Player1First, Player2First){
+  game.play(Player1First);
+  game.play(Player2First);
+}
+
+var round2 = function(game, Player1Second, Player2Second){
+  game.play(Player1Second);
+  game.play(Player2Second);
+}
