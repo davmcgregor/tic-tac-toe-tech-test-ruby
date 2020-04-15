d3359074314ce26efed2e8b6ffb01e14764d3eed
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-04-15 15:25:34 +0100

    Refactor grid logic into the Board object

--- src/Board.js
+++ src/Board.js
@@ -6,6 +6,41 @@
   Board.prototype = {
     isFree: function(position){
       return this.grid[position[0]][position[1]].length === 0;
+    },
+    compareCells: function(cell_one, cell_two){
+      return JSON.stringify(this.grid[cell_one[0]][cell_one[1]]) == JSON.stringify(this.grid[cell_two[0]][cell_two[1]])
+    },
+    compareRow: function(row_number){
+      return this.compareCells([row_number,0], [row_number,1]) && this.compareCells([row_number,1], [row_number,2]);
+    },
+    notEmptyRow: function(row_number){
+      return this.grid[row_number][0][0] == "X" || this.grid[row_number][0][0] == "O";
+    },
+    winningRow: function(row_number){
+      return this.compareRow(row_number) && this.notEmptyRow(row_number);
+    },
+    compareCol: function(col_number){
+      return this.compareCells([0, col_number], [1, col_number]) && this.compareCells([1, col_number], [2, col_number]);
+    },  
+    notEmptyCol: function(col_number){
+      return this.grid[0][col_number][0] == "X" || this.grid[0][col_number][0] == "O";
+    },
+    winningCol: function(col_number){
+      return this.compareCol(col_number) && this.notEmptyCol(col_number)
+    },
+    compareDiagonalLeftRight: function(){
+      return this.compareCells([0,0], [1,1]) && this.compareCells([1,1], [2,2])
+    },
+    compareDiagonalRightLeft: function(){
+      return this.compareCells([0,2], [1,1]) && this.compareCells([1,1], [2,0])
+    },
+    notEmptyCentre: function(){
+      return this.grid[1][1][0] == "X" || this.grid[1][1][0] == "O";
+    },
+    winningDiag: function(){
+      if ((this.compareDiagonalLeftRight() || this.compareDiagonalRightLeft()) && this.notEmptyCentre()) {
+        throw "Game over!";
+      }
     }
   };
 
--- src/Game.js
+++ src/Game.js
@@ -28,53 +28,12 @@
 
     checkWin: function(){
       for (i = 0; i < 3; i++) {
-        if (this.winningRow(i) || this.winningCol(i)) {
+        if (this.board.winningRow(i) || this.board.winningCol(i) || this.board.winningDiag()) {
           throw "Game over!";
         }
       }
-      if (this.compareDiagonalLeftRight() && this.notEmptyCentre()) {
-        throw "Game over!";
-      }
-      if (this.compareDiagonalRightLeft() && this.notEmptyCentre()) {
-        throw "Game over!";
-      }
-    },
-
-    compareCells: function(cell_one, cell_two){
-      return JSON.stringify(this.board.grid[cell_one[0]][cell_one[1]]) == JSON.stringify(this.board.grid[cell_two[0]][cell_two[1]])
-    },
-
-
-    compareRow: function(row_number){
-      return this.compareCells([row_number,0], [row_number,1]) && this.compareCells([row_number,1], [row_number,2]);
-    },
-    notEmptyRow: function(row_number){
-      return this.board.grid[row_number][0][0] == "X" || this.board.grid[row_number][0][0] == "O";
-    },
-    winningRow: function(row_number){
-      return this.compareRow(row_number) && this.notEmptyRow(row_number);
-    },
-
-    compareCol: function(col_number){
-      return this.compareCells([0, col_number], [1, col_number]) && this.compareCells([1, col_number], [2, col_number]);
-    },  
-    notEmptyCol: function(col_number){
-      return this.board.grid[0][col_number][0] == "X" || this.board.grid[0][col_number][0] == "O";
-    },
-    winningCol: function(col_number){
-      return this.compareCol(col_number) && this.notEmptyCol(col_number)
-    },
-
-    compareDiagonalLeftRight: function(){
-      return this.compareCells([0,0], [1,1]) && this.compareCells([1,1], [2,2])
-    },
-    compareDiagonalRightLeft: function(){
-      return this.compareCells([0,2], [1,1]) && this.compareCells([1,1], [2,0])
-    },
-    notEmptyCentre: function(){
-      return this.board.grid[1][1][0] == "X" || this.board.grid[1][1][0] == "O";
     }
-  }
+  };
 
   exports.Game = Game;
 
